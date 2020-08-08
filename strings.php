6c17638e55c3fd88d410b81ed28a94e87bf789d1
WinterCricket <hodhodhod2003@gmail.com>
2020-08-07 23:41:18 -0500

    noodling around with string related functions

--- strings.php
+++ strings.php
@@ -1,8 +1,18 @@
 <?php  
 
 $stringexample = 'Hello World';
+	// echo str_split($stringexample);
+$proxy = str_ireplace("world", "London", $stringexample);
+$reborn = str_split(str_shuffle($proxy));
 
-	echo str_shuffle($stringexample);
+echo join($reborn);
+
+echo str_word_count($stringexample);
+
+echo strlen($reborn);
+
+
+	// echo str_shuffle($stringexample);
   // echo $stringexample . "<br>";
   // echo "Hello World Again <br>";
   // echo 'I\'m Back' . "<br>";
