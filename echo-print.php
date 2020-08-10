471ef6d61a320c2cb582829486e17c1ce1e92b63
WinterCricket <hodhodhod2003@gmail.com>
2020-08-09 22:13:30 -0500

    trying some echo approaches with concat

---
+++ echo-print.php
@@ -0,0 +1,31 @@
+<?php  
+
+echo "Hello World of Existential confusion!" . "<br>";
+echo "Hello"." Confusion!"."<br>";
+echo 10 + 8;
+
+echo "<br>";
+
+$var = "<h1>" . "Hello World of Existential confusion!" . "</h1>";
+
+echo $var;
+
+$var2 = "Hello you miserable fool!";
+
+echo "<h1>" . $var2 . "</h1>";
+
+echo "<br>";
+
+$fname = 'Mica';
+$mname = 'Todd';
+$lname = 'Sorin';
+
+echo $fname .' '. $mname . ' ' . $lname;
+
+echo "<br>";
+
+echo "$fname $mname $lname";
+
+
+
+echo "<br>";
