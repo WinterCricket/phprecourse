2018a32ef8880670fd406acf9dc5569a1366b64f
WinterCricket <hodhodhod2003@gmail.com>
2020-08-10 14:05:41 -0500

    add comments file

---
+++ comments.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<h1>Comments</h1>
+
+<?php
+
+// This is a single line comment
+
+# This is also a single line comment
+
+/*
+ * This is a multiline comment block.
+ * Used to comment out large sections or blocks of code.
+ */
+
+ echo 22 - 4 + 6 ;
+
+ echo "<br>";
+
+ echo 22 /*- 4 */ + 6;
+
+?>
+
+</body>
+</html>
