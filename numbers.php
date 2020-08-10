c42eb7ea0158de699081b76ba2d56796fe22fcc5
WinterCricket <hodhodhod2003@gmail.com>
2020-08-09 21:45:56 -0500

    add conditional checks for string numbers versus int

--- numbers.php
+++ numbers.php
@@ -20,3 +20,20 @@ if($callit != 1){
 	echo "go away now";
 }
 echo "yessum!";
+
+echo "<br>";
+
+//test/conditional checks for numberical string and integer
+
+$test1 = '12345';
+$test2 = 12345;
+
+if ( is_int($test1) ) {
+  echo '$test1 is an Integer <br>';
+} elseif ( is_string($test1)) {
+  echo '$test1 is a String <br>';
+}
+
+if ( is_int($test2) ) {
+  echo '$test2 is a Integer';
+}
